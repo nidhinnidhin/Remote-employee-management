e1bfa237ee39a2d20164923a71645f978b9715c7
Nidhinnidhin <nidhinbabu171@gmail.com>
2026-01-12 18:04:53 +0530

    mongodb configured

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongoDatabaseModule } from './infrastructure/database/mongoose/mongoose.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), MongoDatabaseModule],
 })
 export class AppModule {}
