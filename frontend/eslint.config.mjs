e4442166081cb4c69391688431deaeff28f4e600
Nidhinnidhin <nidhinbabu171@gmail.com>
2026-03-11 12:57:36 +0530

    no eslint errors in frontend

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,15 +1,24 @@
-import { defineConfig } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
+import nextPlugin from "@next/eslint-plugin-next";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
-export default defineConfig({
-  ...nextVitals,
-  ...nextTs,
-  rules: {
-    // Add rules overrides here
-    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-floating-promises': 'error',
+export default [
+  {
+    plugins: {
+      "@next/next": nextPlugin,
+      "@typescript-eslint": tsPlugin,
+    },
+    languageOptions: {
+      parser: tsParser,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/explicit-function-return-type": "off",
+    },
   },
-  ignores: ['.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
-});
+  {
+    ignores: [".next/**", "out/**", "build/**", "next-env.d.ts"],
+  },
+];
